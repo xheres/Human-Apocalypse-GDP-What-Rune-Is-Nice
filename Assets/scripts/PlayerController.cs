5cd04b653ee0d73dfccb28bc28e4d68b91d643f3
xheres <xheresksm@gmail.com>
2015-11-03 23:21:50 +0800

    Initial Upload
    -

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	
+	float duration = 1.5f,
+		  MAX_Y = 0.0f;
+	Vector2 endPoint;
+	bool flag = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButton(0)) 
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) 
+			{
+				flag = true;
+				endPoint = hit.point;
+				Debug.Log (endPoint);
+			}
+
+			if (flag && !Mathf.Approximately (transform.position.magnitude, endPoint.magnitude))
+				transform.position = Vector2.Lerp (transform.position, endPoint, 1 / (duration * (Vector2.Distance (gameObject.transform.position, endPoint))));
+			else if(flag && Mathf.Approximately(transform.position.magnitude, endPoint.magnitude))
+				flag = false;
+		}
+	}
+}
+
