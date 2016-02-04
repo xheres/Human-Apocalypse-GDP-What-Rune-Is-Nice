2402ac98d6438db5b6949607b02836df9898c069
Orghark <calvin_lee_jw@hotmail.com>
2016-02-05 05:06:40 +0800

    Revert "Added my FSM and shit, edited my muzzles"
    This reverts commit 1f1a5b321b54cf9e0e841d068e666c543548c17c.

---
+++ Assets/scripts/_Old/FSM/Boss/FSM/BossFSM.cs
@@ -0,0 +1,102 @@
+﻿//Matric Number: 1401169f, Name: Calvin, Class: P01
+using UnityEngine;
+using System.Collections;
+
+public class BossFSM : MonoBehaviour {
+    //initialize variables
+    GameObject Boss;
+    EnemyProperties enemyProp;
+    int nAmmo;
+    bool changedMove = false;
+public enum eBossState
+    {
+        Spawn,
+        CreateMuzzle,
+        DeleteMuzzle,
+        Attack,
+        Retreat,
+        DeleteEntity,
+        Move
+       
+    }
+
+    BossState m_StateSpawn;
+    BossState m_StateCreateMuzzle;
+    BossState m_StateDeleteMuzzle;
+    BossState m_StateAttack;
+    BossState m_StateRetreat;
+    BossState m_StateDeleteEntity;
+    BossState m_StateMove;
+    BossState m_CurrentState;
+    BossState m_LastState;
+
+    void Start ()
+    {
+        m_StateSpawn = new BossStateSpawn(this);
+        m_StateCreateMuzzle = new BossStateCreateMuzzle(this);
+        m_StateDeleteMuzzle = new BossStateDeleteMuzzle(this);
+        m_StateAttack = new BossStateAttack(this);
+        m_StateRetreat = new BossStateRetreat(this);
+        m_StateDeleteEntity = new BossStateDeleteEntity(this);
+        m_StateMove = new BossStateMove(this);
+
+         Boss = GameObject.Find("Boss_1");
+         enemyProp = Boss.GetComponent<EnemyProperties>();
+
+         ChangeState(eBossState.Spawn);
+    }
+	// Update is called once per frame
+	void Update () //update states
+    {
+        m_CurrentState.Enter();
+
+	}
+
+    public eBossState ReturnNextState() //returns the next state in line
+    {
+        nAmmo = enemyProp.getAmmo();
+        if (m_CurrentState == m_StateSpawn)
+        {
+            return eBossState.CreateMuzzle;
+        }
+        if (m_CurrentState == m_StateCreateMuzzle)
+        {
+            return eBossState.Move;
+        }
+        if (m_CurrentState == m_StateMove)
+        {
+            return eBossState.Attack;
+        }
+        if (m_CurrentState == m_StateAttack)
+        {
+            if (nAmmo > 0)
+            {
+                return eBossState.DeleteMuzzle;
+            }
+            if (nAmmo <= 0)
+            {
+                return eBossState.Retreat;
+            }
+        }
+        if (m_CurrentState == m_StateDeleteMuzzle)
+        {
+            return eBossState.CreateMuzzle;
+        }
+        return eBossState.DeleteEntity;
+    }
+
+    public void ChangeState (eBossState _newState) //change the current state to the selected state
+    {
+        switch (_newState)
+        {
+            case eBossState.Spawn: m_CurrentState = m_StateSpawn; break;
+            case eBossState.CreateMuzzle: m_CurrentState = m_StateCreateMuzzle; break;
+            case eBossState.DeleteMuzzle: m_CurrentState = m_StateDeleteMuzzle; break;
+            case eBossState.Attack: m_CurrentState = m_StateAttack; break;
+            case eBossState.Retreat: m_CurrentState = m_StateRetreat; break;
+            case eBossState.DeleteEntity: m_CurrentState = m_StateDeleteEntity; break;
+            case eBossState.Move: m_CurrentState = m_StateMove; break;
+        }
+        m_CurrentState.Enter();
+    }
+}
---
+++ Assets/scripts/_Old/FSM/Boss/FSM/BossStateCreateMuzzle.cs
@@ -0,0 +1,50 @@
+﻿//Matric Number: 1401169f, Name: Calvin, Class: P01
+using UnityEngine;
+using System.Collections;
+
+public class BossStateCreateMuzzle : BossState {
+
+   
+    [SerializeField] GameObject[] muzzles = new GameObject[3];
+    GameObject prevMuzzle;
+    GameObject chosenMuzzle;
+    GameObject Boss;
+    EnemyProperties enemyProp;
+
+    bool hasEntered = false;
+
+    public BossStateCreateMuzzle(BossFSM _FSM)
+	{
+		m_BossFSM = _FSM;
+	}
+
+    public override void Enter()
+    {
+        if (hasEntered == false)
+        {
+            Boss = GameObject.Find("Boss_1");
+            enemyProp = Boss.GetComponent<EnemyProperties>();
+            hasEntered = true;
+        }
+        Execute();
+    }
+
+    public override void Execute()
+    {
+        int i = Random.Range(0, 3); //random a number between 0-2
+        chosenMuzzle = muzzles[i];
+        while (chosenMuzzle == prevMuzzle) //as long as the chosen muzzle is the same as the prev muzzle, do a random between 0-2 again 
+        {
+            i = Random.Range(0, 3); //random a number between 0-2
+            chosenMuzzle = muzzles[i];
+        }
+        enemyProp.setMuzzle(chosenMuzzle);
+        Debug.Log("Create the muzzle");
+        Exit();
+    }
+
+    public override void Exit()
+    {
+        m_BossFSM.ChangeState(m_BossFSM.ReturnNextState());
+    }
+}
