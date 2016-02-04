66d1228d73d9fc05790bafd116bc5b024b7dc59b
ryukasu <llky123@hotmail.com>
2016-02-05 07:22:18 +0800

    Added boss 04 stuff
    added boss 04 prefab and animation

---
+++ Assets/prefabs/Enemies/_OldButNewer/Projectiles/_New_MadeStuff/Lucas/Boss04Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss04Movement : MonoBehaviour
+{
+    Transform player;
+    public float movespeed;
+
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        player = go.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = new Vector3(player.transform.position.x * movespeed, transform.position.y, transform.position.z);
+
+
+    }
+}
